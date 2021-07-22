858388baab53730033ad5f308cd701c1f3ca4800
SURFACEBOOK\JAVA10 <563622979@QQ.COM>
2021-07-22 18:40:45 +0800

    Just test item&block

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,14 +1,31 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ExampleMod implements ModInitializer {
+
+	public static final Item TEST_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+
+	public static final Block TEST_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("test","test_item"), TEST_ITEM);
+		Registry.register(Registry.BLOCK, new Identifier("test","test_block"), TEST_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier("test","test_block"), new BlockItem(TEST_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
+
 	}
 }
