39ab88ea5c7fb59962747ce56a7fef693ca6f33c
SURFACEBOOK\JAVA10 <563622979@QQ.COM>
2021-07-25 18:18:52 +0800

    测试结束，我完全理解了一切

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,15 +1,23 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
+import net.minecraft.client.color.block.BlockColorProvider;
+import net.minecraft.client.color.item.ItemColorProvider;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.BlockRenderView;
+import org.jetbrains.annotations.Nullable;
 
 public class ExampleMod implements ModInitializer {
 
@@ -23,7 +31,20 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+
+		ColorProviderRegistry.ITEM.register(new ItemColorProvider() {
+			@Override
+			public int getColor(ItemStack stack, int tintIndex) {
+				return 0x228B22;
+			}
+		}, TEST_ITEM);
 		Registry.register(Registry.ITEM, new Identifier("test","test_item"), TEST_ITEM);
+		ColorProviderRegistry.BLOCK.register(new BlockColorProvider() {
+			@Override
+			public int getColor(BlockState state, @Nullable BlockRenderView world, @Nullable BlockPos pos, int tintIndex) {
+				return 0xFFED97;
+			}
+		}, TEST_BLOCK);
 		Registry.register(Registry.BLOCK, new Identifier("test","test_block"), TEST_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier("test","test_block"), new BlockItem(TEST_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
 
